065608c83e33352f0a21ba4955e9a55eba4f8625
Joseph Francis <josephfrancis100@gmail.com>
2019-07-18 23:25:04 -0500

    More info

--- app.py
+++ app.py
@@ -18,5 +18,45 @@ address.append("Something")
 print(address)
 address.remove("Something")
 print(address)
-address.pop(1)
+address.pop(1)  # removes by index
 print(address)
+
+# Dictionaries
+key_val = {"Mike": 1234, "Joseph":324, "There": 12}
+print(key_val["There"])
+print(key_val["Joseph"])
+
+keys = ["a", "b", "c"]
+values = [1, 2, 3]
+
+combined = dict(zip(keys, values))
+print(combined)
+
+val_from_user = int(input("Enter the number: "))
+# print("The squared result is: " + str(val_from_user ** 2))
+
+if val_from_user in key_val.values():
+    print("The value matched!")
+else:
+    print("The value did not match")
+
+# elif = else if
+
+
+def get_age(year):
+    return 2019 - year
+
+
+def find_in_file(fruit):
+    my_file = open("sample.txt")
+    fruits = my_file.read().splitlines()
+    print(fruit, fruits)
+    if fruit in fruits:
+        return "The fruit is in the list"
+    else:
+        return "This fruit is not in the list"
+
+
+print(find_in_file("Orange"))
+
+
